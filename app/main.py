fe008d45b3d7042c098370ecc64bed9fc77f405a
Yash Jagdale <128350613+YashJagdale2122@users.noreply.github.com>
2026-01-30 11:14:29 +0530

    chore: create database tables on application startup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.routes import videos
-
+from app.core.database import engine, Base
 app = FastAPI (title = "Video Intelligence Backend", version="1.0.0")
 
 app.include_router(videos.router)
@@ -8,3 +8,7 @@ app.include_router(videos.router)
 @app.get("/api/v1/health")
 def health_check():
     return {"status" : "Ok"}
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
