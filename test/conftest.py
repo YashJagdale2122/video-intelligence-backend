1be92248abc6cc89d050f32346ca98cf464bebba
Yash Jagdale <128350613+YashJagdale2122@users.noreply.github.com>
2026-01-30 15:28:54 +0530

    test: fix pytest path resolution for CI

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
-sys.path.append(str(PROJECT_ROOT))
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(PROJECT_ROOT))
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
